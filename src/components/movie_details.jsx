5c8168df2501c4461191698a97b2b1ff2b2beff5
Jimmy Li <jimmyli921@gmail.com>
2018-01-10 21:24:47 -0800

    update movie details page

--- src/components/movie_details.jsx
+++ src/components/movie_details.jsx
@@ -9,25 +9,51 @@ class MovieDetails extends React.Component {
     this.props.fetchMovieDetails(id);
   }
 
-
+  formatTagLine(tagline) {
+  }
 
   render() {
-    const url = "https://image.tmdb.org/t/p/";
-    let display = null;
-    let style = null;
     const {details} = this.props;
+    let display = null, style = null, imdb = null;
+    const url = "https://image.tmdb.org/t/p/";
     if(details) {
       style = {
         backgroundImage: "url(" + url + "original" + details.backdrop_path + ")"
       };
+      imdb = `https://www.imdb.com/title/${details.imdb_id}`;
       display = (
-        <div className="movie-details-overlay">
-          <div className="movie-details">
-            <div className="movie-poster-wrapper">
-              <img className="movie-poster"src={url + "w500" + details.poster_path}></img>
-            </div>
-            <div className="detail-content">
-
+        <div className="movie-details">
+          <img className="movie-poster" src={url + "w500" + details.poster_path} alt="movie-poster"></img>
+          <div className="detail-content">
+            <h2>{details.title.toUpperCase()}</h2>
+            {
+              details.tagline.length ? <h3>{details.tagline}</h3> : null
+            }
+            <p>{details.overview}</p>
+            <div className="content-details">
+              <div className="content-section">
+                <h4>Run Time</h4>
+                <h2>{details.runtime} minutes</h2>
+              </div>
+              <div className="content-section">
+                <h4>Vote Average</h4>
+                <h2>{details.vote_average}/10</h2>
+              </div>
+              <div className="content-section">
+                <h4>Box Office</h4>
+                <h2>
+                  ${details.revenue.toLocaleString()}
+                </h2>
+              </div>
+              <div className="content-section">
+                <h4>Release Date</h4>
+                <h2>
+                  {details.release_date}
+                </h2>
+              </div><div className="content-section">
+                <h4>IMDb</h4>
+                <h2><a href={imdb}>{imdb.slice(8)}</a></h2>
+              </div>
             </div>
           </div>
         </div>
@@ -38,7 +64,9 @@ class MovieDetails extends React.Component {
 
     return(
       <div style={style} className="movie-details-container">
-        {display}
+        <div className="movie-details-overlay">
+          {display}
+        </div>
       </div>
     );
   }
